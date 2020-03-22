35362d21d2c3dae4189bf47197a3d544153f5ee3
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-22 00:33:36 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,8 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-  def call(env)
-    resp = Rack::Response.new
+    num_1 = Kernel.rand(1..12)
+    num_2 = Kernel.rand(12..24)
  
       if Time < 12
         resp.write "Good Morning!"
