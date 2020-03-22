c3e06eb63723d196afe008cb732cd8643e723f3a
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-22 00:41:58 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,9 +9,9 @@ class Application
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
  
-    if num_1 <= num_2 
+    if num_1 > num_2 
       resp.write "Good Morning!"
-    elsif num_1 > num_2
+    elsif num_1 <= num_2
       resp.write "Good Afternoon!"
     end
  
